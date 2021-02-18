58d0d32510f39633c87f11c54092762fb9b53ae4
Jun Santos <xjunsantos@gmail.com>
2021-02-18 16:24:08 +0800

    Classified links

--- app/Models/Classified.php
+++ app/Models/Classified.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Classified extends Model
 {
@@ -12,5 +13,10 @@ class Classified extends Model
     public function classified_image()
     {
         return $this->hasMany(ClassifiedImage::class);
+    }
+
+    public function getSlugAttribute(): string
+    {
+        return Str::slug($this->title);
     }
 }
